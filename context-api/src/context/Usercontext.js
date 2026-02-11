6440c2c0307f47bc7de2a12d1043c296cf915503
waleedmurad91 <waleedmurad912gmail.com>
2026-02-11 12:35:48 +0500

    second branch

--- context-api/src/context/Usercontext.js
+++
@@ -1,8 +0,0 @@
-// import React from 'react'
-import { createContext } from 'react'
-
-// const UserContext = React.createContext;
-
-const UserContext = createContext();
-
-export default UserContext;
